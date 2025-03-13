6132b572bad9589ac8c94cac358d9ef8a90f9d4a
truyenk4 <nhgtruyen@gmail.com>
2025-03-13 10:31:23 +0700

    add vigenere

--- LAB-02/ex01/api.py
+++ LAB-02/ex01/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
+from cipher.vigenere import VigenereCipher
 app = Flask(__name__)
 
 #CAESAR CIPHER ALGGRITHM
@@ -20,6 +21,25 @@ def caesar_decrypt():
     key = int(data['key'])
     decrypt_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypt_text})
+
+vigenere_cipher = VigenereCipher()
+
+@app.route('/api/vigenere/encrypt', methods=["POST"])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/vigenere/decrypt', methods=["POST"])
+def vigenere_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+    
 
 #main function
 if __name__ == "__main__":
--- LAB-02/ex01/cipher/vigenere/vigenere_cipher.py
+++ LAB-02/ex01/cipher/vigenere/vigenere_cipher.py
@@ -9,4 +9,26 @@ class VigenereCipher:
             if char.isalpha():
                 key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
                 if char.isupper():
-                    encrypted_text += chr((ord(char) - ord('A')))
+                    encrypted_text += chr((ord(char) - ord('A') + key_shift) % 26 + ord('A'))
+                else:
+                    encrypted_text += chr((ord(char) - ord('a') + key_shift) % 26 + ord('a'))
+                key_index += 1
+            else:
+                encrypted_text += char
+        return encrypted_text
+    
+    def vigenere_decrypt(self, encrypted_text, key):
+        decrypted_text = ""
+        key_index = 0
+        for char in encrypted_text:
+            if char.isalpha():
+                key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
+                if char.isupper():
+                    decrypted_text += chr((ord(char) - ord('A') - key_shift) % 26 + ord('A'))
+                else:
+                    decrypted_text += chr((ord(char) - ord('a') - key_shift) % 26 + ord('a'))
+                key_index += 1
+            else:
+                decrypted_text += char
+        return decrypted_text
+                
