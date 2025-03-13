d7e0c5a974bef7754fe04f25718f7710463665fa
truyenk4 <nhgtruyen@gmail.com>
2025-03-13 09:55:20 +0700

    add lab02 ex01 caesar

---
+++ LAB-02/ex01/cipher/vigenere/vigenere_cipher.py
@@ -0,0 +1,12 @@
+class VigenereCipher:
+    def __init__(self):
+        pass
+    
+    def vigenere_encrypt(self, plain_text, key):
+        encrypted_text = ""
+        key_index = 0
+        for char in plain_text:
+            if char.isalpha():
+                key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
+                if char.isupper():
+                    encrypted_text += chr((ord(char) - ord('A')))
